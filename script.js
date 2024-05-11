b6e11d3ce31cd814d06f5e608d57ca6c94c41236
AkmaljonYusupov <infoakmaljon.uz@gmail.com>
2024-05-11 21:04:09 +0500

    footer fixed menu end

--- script.js
+++ script.js
@@ -52,3 +52,15 @@ const swiper = new Swiper('.swiper', {
 		prevEl: '.swiper-button-prev',
 	},
 })
+
+// show search
+const searchButton = document.querySelector('.t-search'),
+	tClose = document.querySelector('.search-close'),
+	showClass = document.querySelector('.site')
+searchButton.addEventListener('click', function () {
+	showClass.classList.toggle('showsearch')
+})
+
+tClose.addEventListener('click', function () {
+	showClass.classList.remove('showsearch')
+})
